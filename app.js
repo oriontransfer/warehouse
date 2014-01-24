3057d8bed93c000512875f20c4c0baf986dd2083
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2014-01-25 11:12:52 +1300

    Add news handling code.

--- app.js
+++ app.js
@@ -17,5 +17,11 @@ var io = socketio.listen(server);
 io.sockets.on('connection', function (socket) {
 	socket.on('handleEvent', function(data) {
 		console.log(data);
+		
+		socket.emit("eventHandled");
 	});
+	
+	setInterval(function() {
+		socket.emit('news', { time: new Date() + '' });
+	}, 1000);
 });
