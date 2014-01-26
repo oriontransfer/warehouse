636db442a79bb4b785c63b0429e1ae69ce9348b2
r3david <deadtheory84@gmail.com>
2014-01-26 16:31:03 +1300

    *Added shelves

--- public/geometry.js
+++ public/geometry.js
@@ -177,3 +177,84 @@ WallController.prototype.generate = function()
 	}
 	
 }
+
+
+function ShelvesController (scene, region, density, worldState)
+{
+	this.scene 		= scene;
+	this.region 	= region;
+	this.worldState = worldState;
+	this.density 	= density;
+	this.random 	= new RandomGenerator(612351232);
+	
+	this.shelves_assets = [
+		AngryBox.assets.get('shelf-long-boxes'),
+		AngryBox.assets.get('shelf-long-crates'),
+		AngryBox.assets.get('shelf-long-barrels'),
+		AngryBox.assets.get('barrel-blue'),
+		AngryBox.assets.get('barrel-red')
+	];
+	
+	this.shelves = [
+		0,
+		1,
+		0,
+		2,
+		0,
+		0,
+		3,
+		2,
+		0,
+		1,
+		0,
+		3,
+		4,
+		0,
+		4,
+		0,
+	];		
+	
+	this.shelve_size = 8;
+	this.grid_width  = Math.floor( (this.region[1].x - this.region[0].x)/this.shelve_size );
+	this.grid_height = Math.floor( (this.region[1].y - this.region[0].y)/this.shelve_size );
+}
+
+ShelvesController.prototype.generateShelveAtPosition = function(pos_x, pos_y, rot_vec, angle, type_of_shelve){
+	var shelve =  this.shelves_assets[this.shelves[type_of_shelve]];
+	var mesh = new THREE.Mesh(shelve.geometry, shelve.material);
+	mesh.castShadow    = true;
+	mesh.receiveShadow = true;
+	mesh.position.x = pos_x;
+	mesh.position.y = pos_y;
+	mesh.rotateOnAxis(rot_vec, D2R(angle));
+	var boxsize = (angle == 270 || angle == 0 ? new CANNON.Vec3(this.shelve_size/2,this.shelve_size/3,100) : new CANNON.Vec3(this.shelve_size/3,this.shelve_size/2,100) );
+	var mass =  this.shelves[type_of_shelve] == 3 || this.shelves[type_of_shelve] == 4 ? 0  : 0;
+	this.worldState.addBoxGeometry(new CANNON.Vec3(mesh.position.x,mesh.position.y,0),boxsize, mass, "");
+	this.scene.add(mesh);
+}
+
+ShelvesController.prototype.generateSingleHorizontalLine = function(from_x, to_x, y){
+	for(var j=from_x; j<to_x; j++){
+		this.generateShelveAtPosition(j*this.shelve_size, y*this.shelve_size, (new THREE.Vector3(0, 0, 1)).normalize(), 270,this.random.nextInteger(10000)%this.shelves.length);
+	}
+}
+
+ShelvesController.prototype.generateHorizontalLines = function()
+{	
+	var n_shelve_lines_max = this.grid_height/2;
+	var n_shelve_lines_min = 1;
+	var n_shelve_lines     = Math.floor( n_shelve_lines_max*this.density + n_shelve_lines_min*(1-this.density) );
+	var start_idx_x        = 2;
+	var end_idx_x		   = this.grid_width - 2;
+	var half_x 			   = (end_idx_x - start_idx_x)/2
+	var dy 				   = Math.floor(this.grid_height / n_shelve_lines); 
+	
+	for(var i=1; i<=n_shelve_lines; i++){
+		var shelve =  this.shelves[0];
+		for(var j=start_idx_x; j<end_idx_x; j++){
+			if(j>half_x-2 && j <half_x+1) continue;
+			
+			this.generateShelveAtPosition(this.region[0].x + j*this.shelve_size, this.region[0].y + i*dy*this.shelve_size, (new THREE.Vector3(0, 0, 1)).normalize(), 270,this.random.nextInteger(10000)%this.shelves.length);
+		}
+	}
+}
--- public/worldcontroller.js
+++ public/worldcontroller.js
@@ -48,6 +48,9 @@ function WorldController() {
 	this.wallController = new WallController(this.scene, [32, 12]);
 	this.wallController.generate();
 	
+	this.shelveController = new ShelvesController(this.scene, [new CANNON.Vec3(2,2,0), new CANNON.Vec3(32*8,12*8,0)], 1, this.worldState);
+	this.shelveController.generateHorizontalLines();	
+	
 	//WALLS COLLISION
 	this.worldState.addBoxGeometry(new CANNON.Vec3(-5,12*4,0), new CANNON.Vec3(1,12*4+4,100), 0, "");//left
 	this.worldState.addBoxGeometry(new CANNON.Vec3(32*8-3,12*4,0), new CANNON.Vec3(1,12*4+4,100), 0, "");//right
