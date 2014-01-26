9e93371e0058fe8b0ac63b149145621b78860d9e
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2014-01-26 22:45:45 +1300

    Mucking around with physics.

--- public/worldstate.js
+++ public/worldstate.js
@@ -44,17 +44,20 @@ function WorldState() {
 	}.bind(this);
 	
 	this.players.deserializeObject = function(object, key, data) {
-		object.deserialize(data);
+		//console.log("current", object.serialize());
+		//console.log("updated", data);
 		
-		this.world.remove(object.rigidBody);
-		var body = object.rigidBody;
+		//object.deserialize(data);
+		
+		//this.world.remove(object.rigidBody);
+		//var body = object.rigidBody;
 		
 		// We forcefully update the state of the rigid body:
-		body.position.copy(body.initPosition);
-		body.velocity.copy(body.initVelocity);
-		body.quaternion.copy(body.initQuaternion);
+		//body.position.copy(body.initPosition);
+		//body.velocity.copy(body.initVelocity);
+		//body.quaternion.copy(body.initQuaternion);
 		
-		this.world.add(object.rigidBody);
+		//this.world.add(object.rigidBody);
 	}.bind(this);
 	
 	this.projectiles = new Container();
@@ -112,40 +115,22 @@ WorldState.prototype.initPhysics = function(){
 	//Initialise the world
 	this.world = new CANNON.World();
 	this.world.broadphase = new CANNON.NaiveBroadphase();
-	world = this.world;
 	this.world.broadphase.useBoundingBoxes = true;
+	
+	console.log("Creating physics world...");
+	
+	var solver = new CANNON.GSSolver();
+	solver.iterations = 20;
+	solver.tolerance = 0;
 
-	solver = new CANNON.GSSolver();
-
-	// world.defaultContactMaterial.contactEquationStiffness = 1e8;
- //    world.defaultContactMaterial.contactEquationRegularizationTime = 3;
-
-    solver.iterations = 20;
-    solver.tolerance = 0;
-
-    world.solver = new CANNON.SplitSolver(solver);
-
-    world.gravity.set(0, 0, -9.8);
+	this.world.solver = new CANNON.SplitSolver(solver);
+	this.world.gravity.set(0, 0, -9.8);
 
 	//Initialise the physics contact materials.
 	this.boxPhysicsMaterial = new CANNON.Material("BOX_PHY_MATERIAL");
-	// this.boxPhysicsContactMaterial = new CANNON.ContactMaterial(this.boxPhysicsMaterial,
-	// 															this.boxPhysicsMaterial,
-	// 															0,
-	// 															0);
 
 	this.groundPhysicsMaterial = new CANNON.Material("GROUND_PHY_MATERIAL");
-	this.groundPhysicsContactMaterial = new CANNON.ContactMaterial(this.groundPhysicsMaterial,
-																this.boxPhysicsMaterial,
-																0.0005,
-																.3);
-
-	// this.boxPhysicsContactMaterial.frictionEquationStiffness = 1e8;
-	// this.boxPhysicsContactMaterial.frictionEquationRegularizationTime = 3;
-
-	// // Adjust constraint equation parameters
- //    this.boxPhysicsContactMaterial.contactEquationStiffness = 1e8;
- //    this.boxPhysicsContactMaterial.contactEquationRegularizationTime = 3;
+	this.groundPhysicsContactMaterial = new CANNON.ContactMaterial(this.groundPhysicsMaterial, this.boxPhysicsMaterial, 0.0005, 0.3);
 
 	// Adjust constraint equation parameters
    	this.groundPhysicsContactMaterial.contactEquationStiffness = 1e8;
@@ -155,16 +140,18 @@ WorldState.prototype.initPhysics = function(){
 	this.groundPhysicsContactMaterial.frictionEquationRegularizationTime = 3;
 
 	//orld.addContactMaterial(this.boxPhysicsContactMaterial);
-	world.addContactMaterial(this.groundPhysicsContactMaterial);
+	this.world.addContactMaterial(this.groundPhysicsContactMaterial);
 
 	//Initialise the ground plane
 	var groundShape = new CANNON.Plane();
 	var groundBody = new CANNON.RigidBody(0, groundShape, this.groundPhysicsMaterial);
+	
 	groundBody.quaternion.setFromAxisAngle(new CANNON.Vec3(0,0,1),-Math.PI/2);
-	world.add(groundBody);
+	
+	this.world.add(groundBody);
 }
 
-WorldState.prototype.update = function(dt){
+WorldState.prototype.update = function(dt) {
 	this.world.step(dt);
 
 	this.players.forEach(function(player){
@@ -186,33 +173,19 @@ WorldState.prototype.addBoxGeometry = function(locationVEC3, halfExtentsVEC3, ma
 
 WorldState.prototype.createPlayer = function(position) {
 	var newPlayer = new PlayerState(this.nextUniqueID++, position, this.boxPhysicsMaterial);
-
+	
 	this.players.push(newPlayer);
-
+	
 	return newPlayer;
 }
 
-
 //Starting location and direction is a Vec3
 WorldState.prototype.addProjectileState = function (startingLocation, startingSpeed ,startingDirection, emittedFrom){
-	var newProjectileState = new ProjectileState(this.nextUniqueID++, startingLocation, startingSpeed, startingDirection, emittedFrom);
-	this.projectiles.push(newProjectileState);
+	var newProjectile = new ProjectileState(this.nextUniqueID++, startingLocation, startingSpeed, startingDirection, emittedFrom);
 	
-	// projShape = new CANNON.Sphere(WorldState.PROJECTILE_SIZE_HALF);
-	// projBody = new CANNON.RigidBody(WorldState.PROJECTILE_MASS, projShape, this.boxPhysicsMaterial);
-	// projBody.position.set(startingLocation[0], startingLocation[1], startingLocation[2]);
-
-	//Get the direciton of the projectile, mult by speed and set that as the initial velocity of the projectile.
-	// var initVelocity = new CANNON.Vec3(0,0,0);
-	// startingDirection.copy(initVelocity);
-	// initVelocity.mult(startingSpeed);
-	// projBody.initVelocity = initVelocity;
-
+	this.projectiles.push(newProjectile);
 	
-	// projBody.userData = newProjectileState;
-	// newProjectileState.rigidBody = projBody;
-
-	// this.world.add(projBody);
+	return newProjectile;
 }
 
 function ProjectileState(ID, startingLocation, speed, direction, emittedFrom) {
@@ -249,17 +222,13 @@ ProjectileState.ORIGIN = new CANNON.Vec3(0,0,0); //constant used for distance ca
 //ProjectileState.LIFETIME_MS = 0.5;
 ProjectileState.KNOCK_BACK = 40000;
 ProjectileState.DAMAGE = 30;
-ProjectileState.bodiesToIntersect = new Array();
-ProjectileState.prototype.update = function(dt){
 
-	//Fill the bodies to intersect array
-	while(ProjectileState.bodiesToIntersect.length > 0){
-		ProjectileState.bodiesToIntersect.pop(); //Clear the array
-	}
+ProjectileState.prototype.update = function(dt) {
+	var bodiesToIntersect = [];
 
 	//Copy only the player bodies into the list for intersection
-	this.worldInside.players.forEach(function(player){
-		ProjectileState.bodiesToIntersect.push(player.rigidBody);
+	this.worldInside.players.forEach(function(player){ 
+		bodiesToIntersect.push(player.rigidBody);
 	});
 
 	this.position.copy(ProjectileState.ORIGIN);
@@ -267,7 +236,7 @@ ProjectileState.prototype.update = function(dt){
 	this.ray.z = 0;
 
 	var ray = new CANNON.Ray(ProjectileState.ORIGIN, this.ray);
-	var intersections = ray.intersectBodies(ProjectileState.bodiesToIntersect);
+	var intersections = ray.intersectBodies(bodiesToIntersect);
 	
 	for(var i = 0; i < intersections.length; i+=1){
 		if(intersections[i] != this.bodyEmittedFrom){
@@ -298,9 +267,9 @@ ProjectileState.prototype.update = function(dt){
 
 // ** Player State **
 
-
-
 function PlayerState(ID, position, material) {
+	console.log("PlayerState:", ID, position, material);
+	
 	this.ID = ID;
 
 	//Positional
@@ -322,6 +291,10 @@ function PlayerState(ID, position, material) {
 	this.isReloading = false;
 	this.roundsInClip = PlayerState.CLIP_SIZE;
 	this.timeSpentReloading = 0;
+	
+	this.lastShotTime = 0;
+	
+	this.combinedDirectionBuffer = new CANNON.Vec3(0,0,0);
 
 	this.worldInside = null;
 
@@ -334,7 +307,7 @@ function PlayerState(ID, position, material) {
 }
 
 PlayerState.prototype.serialize = function() {
-	var p = this.rigidBody.position, q = this.rigidBody.quaternion, v = this.rigidBody.velocity;
+	var p = this.rigidBody.position, q = this.rigidBody.quaternion, v = this.rigidBody.velocity, tau = this.rigidBody.tau;
 	
 	return [
 		p.x, p.y, p.z,
@@ -416,19 +389,14 @@ PlayerState.prototype.setRotationState = function(state, direction) {
 * Function that updates the player state variables.
 */
 PlayerState.ORIGIN = new CANNON.Vec3(0,0,0); //constant used for distance calculations
-PlayerState.combinedDirectionBuffer = new CANNON.Vec3(0,0,0);//Combined direction stores a direction based on key input.
-PlayerState.combinedDirection = new CANNON.Vec3(0,0,0);
 PlayerState.FORWARD = new CANNON.Vec3(0,1,0);
-PlayerState.finalImpulseDir = new CANNON.Vec3(0,0,0);
-PlayerState.lastShotTime = 0;
 
 PlayerState.prototype.handleEvent = function(event, action){
-	var motionState = null
+	var motionState = null;
 	var motionDirection = null;
 	var eventType = null;
-	if(action){
-		motionState = PlayerState.Motion.WALKING;
-	}
+	
+	if (action) motionState = PlayerState.Motion.WALKING;
 	else motionState = PlayerState.Motion.STOPPED;
 
 	switch(event){
@@ -476,7 +444,6 @@ PlayerState.prototype.handleEvent = function(event, action){
 			else this.isShooting = false;
 		break;
 		case EventType.FAST:
-			//console.log("FAST ");
 			if(action) this.isRunning = true;
 			else this.isRunning = false;
 		break;
@@ -497,11 +464,11 @@ PlayerState.prototype.update = function(dt){
 			}
 			this.timeSpentReloading += dt;
 		}
-		else if(PlayerState.FIRE_RATE_PER_SECOND < PlayerState.lastShotTime){
-			bulletDirection = this.rotationQuat.vmult(PlayerState.FORWARD);
+		else if(PlayerState.FIRE_RATE_PER_SECOND < this.lastShotTime){
+			var bulletDirection = this.rotationQuat.vmult(PlayerState.FORWARD);
 			//bulletPosition = this.rigidBody.position.vadd(bulletDirection.mult(2.0));
 			this.worldInside.addProjectileState(this.rigidBody.position, PlayerState.BULLET_SPEED, bulletDirection, this);
-			PlayerState.lastShotTime = 0;
+			this.lastShotTime = 0;
 			this.roundsInClip -= 1;
 			if(this.roundsInClip < 1){
 				console.log('Reloading clip');
@@ -510,49 +477,51 @@ PlayerState.prototype.update = function(dt){
 			}
 		}
 	}
-	PlayerState.lastShotTime += dt;
-	//PlayerState.combinedDirectionBuffer.set(0,0,0);
-
+	
+	this.lastShotTime += dt;
+	
+	this.combinedDirectionBuffer.set(0, 0, 0);
+	
 	switch(this.motionDirection){
 		case PlayerState.Direction.FORWARD:
-			if(this.motion != PlayerState.Motion.STOPPED)PlayerState.combinedDirectionBuffer.y = 1;
-			else PlayerState.combinedDirectionBuffer.y = 0;
+			if(this.motion != PlayerState.Motion.STOPPED)this.combinedDirectionBuffer.y = 1;
+			else this.combinedDirectionBuffer.y = 0;
 		break;
 		case PlayerState.Direction.BACKWARD:
-			if(this.motion != PlayerState.Motion.STOPPED)PlayerState.combinedDirectionBuffer.y = -1;
-			else PlayerState.combinedDirectionBuffer.y = 0;
+			if(this.motion != PlayerState.Motion.STOPPED)this.combinedDirectionBuffer.y = -1;
+			else this.combinedDirectionBuffer.y = 0;
 		break;
 		case PlayerState.Direction.LEFT:
-			if(this.motion != PlayerState.Motion.STOPPED)PlayerState.combinedDirectionBuffer.x = -1;
-			else PlayerState.combinedDirectionBuffer.x = 0;
+			if(this.motion != PlayerState.Motion.STOPPED)this.combinedDirectionBuffer.x = -1;
+			else this.combinedDirectionBuffer.x = 0;
 		break;
 		case PlayerState.Direction.RIGHT:
-			if(this.motion != PlayerState.Motion.STOPPED)PlayerState.combinedDirectionBuffer.x = 1;
-			else PlayerState.combinedDirectionBuffer.x = 0;
+			if(this.motion != PlayerState.Motion.STOPPED)this.combinedDirectionBuffer.x = 1;
+			else this.combinedDirectionBuffer.x = 0;
 		break;
 	}
 
-	PlayerState.combinedDirectionBuffer.normalize();
+	this.combinedDirectionBuffer.normalize();
 
-	//this.rotationQuat.vmult(PlayerState.combinedDirectionBuffer, PlayerState.combinedDirection);
+	//this.rotationQuat.vmult(this.combinedDirectionBuffer, PlayerState.combinedDirection);
 
 	if(this.motion == PlayerState.Motion.WALKING){
-		var impulseDirection = new CANNON.Vec3(0,0,0);
+		var impulseDirection = new CANNON.Vec3(0,0,0), finalImpulseDir = new CANNON.Vec3(0, 0, 0);
 		//this.position.copy(impulseDirection);
 
 		//impulseDirection = impulseDirection.vsub(this.direction);
-		impulseDirection = impulseDirection.vadd(PlayerState.combinedDirectionBuffer);
+		impulseDirection = impulseDirection.vadd(this.combinedDirectionBuffer);
 		impulseDirection = impulseDirection.mult(((this.isRunning) ? PlayerState.RUNNING_SPEED : PlayerState.WALKING_SPEED));
 
-		this.rotationQuat.vmult(impulseDirection, PlayerState.finalImpulseDir);
+		this.rotationQuat.vmult(impulseDirection, finalImpulseDir);
 
-		var finalLength = this.rigidBody.velocity.vadd(PlayerState.finalImpulseDir).distanceTo(PlayerState.ORIGIN);
+		var finalLength = this.rigidBody.velocity.vadd(finalImpulseDir).distanceTo(PlayerState.ORIGIN);
 		var position = new CANNON.Vec3(0,0);
 		this.position.copy(position);
 		//position.y -= 0.5;
 		///this.rigidBody.applyForce(new CANNON.Vec3(0,0,10000), this.position);
-		if(finalLength < ((!this.isRunning) ? PlayerState.MAX_WALKING_SPEED : PlayerState.MAX_RUNNING_SPEED))this.rigidBody.applyForce(PlayerState.finalImpulseDir, position);
-
+		if(finalLength < ((!this.isRunning) ? PlayerState.MAX_WALKING_SPEED : PlayerState.MAX_RUNNING_SPEED))
+			this.rigidBody.applyForce(finalImpulseDir, position);
 	}
 
 	if(this.rotation == PlayerState.Motion.WALKING){
@@ -565,7 +534,7 @@ PlayerState.prototype.update = function(dt){
 		//this.rigidBody.applyImpulse(PlayerState.WALKING_SPEED, this.position);
 	}
 
-	if(this.motion == PlayerState.Motion.STOPPED && PlayerState.combinedDirectionBuffer.y == 0 && PlayerState.combinedDirectionBuffer.x == 0){
+	if(this.motion == PlayerState.Motion.STOPPED && this.combinedDirectionBuffer.y == 0 && this.combinedDirectionBuffer.x == 0){
 			this.rigidBody.velocity = new CANNON.Vec3(0,0,0);
 	}
 
