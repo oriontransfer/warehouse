fd0238751d2d12f8b743361e029b58a91431e4a9
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2016-01-30 14:44:03 +1300

    Additional assets

---
+++ assets/AngryBoxAssets (3)/wallInnerCorner/model.js
@@ -0,0 +1,47 @@
+{
+
+	"metadata" :
+	{
+		"formatVersion" : 3.1,
+		"generatedBy"   : "Blender 2.65 Exporter",
+		"vertices"      : 6,
+		"faces"         : 2,
+		"normals"       : 3,
+		"colors"        : 0,
+		"uvs"           : [8],
+		"materials"     : 1,
+		"morphTargets"  : 0,
+		"bones"         : 0
+	},
+
+	"scale" : 1.000000,
+
+	"materials" : [	{
+		"DbgColor" : 15658734,
+		"DbgIndex" : 0,
+		"DbgName" : "default",
+		"vertexColors" : false
+	}],
+
+	"vertices" : [4,-3.5,2,4,-4,2,3.5,-4,2,4,-3.5,10,4,-4,10,3.5,-4,10],
+
+	"morphTargets" : [],
+
+	"normals" : [0.707083,-0.707083,0,1,0,0,0,-1,0],
+
+	"colors" : [],
+
+	"uvs" : [[0.853804,0.024681,0.786281,0.02468,0.787531,0.846569,0.855054,0.846569,0.784745,0.020082,0.855239,0.020082,0.856544,0.848668,0.78605,0.848668]],
+
+	"faces" : [43,1,0,3,4,0,0,1,2,3,0,1,1,0,43,4,5,2,1,0,4,5,6,7,0,2,2,0],
+
+	"bones" : [],
+
+	"skinIndices" : [],
+
+	"skinWeights" : [],
+
+	"animation" : {}
+
+
+}
---
+++ assets/AngryBoxAssets/floor/floorFlat.js
@@ -0,0 +1,47 @@
+{
+
+	"metadata" :
+	{
+		"formatVersion" : 3.1,
+		"generatedBy"   : "Blender 2.65 Exporter",
+		"vertices"      : 4,
+		"faces"         : 1,
+		"normals"       : 1,
+		"colors"        : 0,
+		"uvs"           : [4],
+		"materials"     : 1,
+		"morphTargets"  : 0,
+		"bones"         : 0
+	},
+
+	"scale" : 1.000000,
+
+	"materials" : [	{
+		"DbgColor" : 15658734,
+		"DbgIndex" : 0,
+		"DbgName" : "default",
+		"vertexColors" : false
+	}],
+
+	"vertices" : [4,-4,0,-4,-4,0,4,4,0,-4,4,0],
+
+	"morphTargets" : [],
+
+	"normals" : [0,0,1],
+
+	"colors" : [],
+
+	"uvs" : [[0,0,1,0,1,1,0,1]],
+
+	"faces" : [43,1,0,2,3,0,0,1,2,3,0,0,0,0],
+
+	"bones" : [],
+
+	"skinIndices" : [],
+
+	"skinWeights" : [],
+
+	"animation" : {}
+
+
+}
